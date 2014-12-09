a791b9872fd1e6cd4967cb1029159263b044f13f
Iñaki García Blasco <txorua@txorua.com>
2014-12-09 12:49:35 +0100

    Creo plantilla para la lista de departamantos. Incluyo una imagen a los campos del vocabulario Departamentos

---
+++ templates/views-view-fields--departamentos--page.tpl.php
@@ -0,0 +1,13 @@
+<?php dsm($row); ?>
+<div class="col-md-4">
+<header>
+<h1><?php print $variables['fields']['name_i18n']->content; ?></h1>
+</header>
+<?php if($row->field_field_imagen): ?>
+<?php
+  $uri = $row->field_field_imagen[0]['raw']['uri'];
+  $src = image_style_url('imagen_400x220', $uri);
+?>
+  <img src="<?php print $src; ?>" class="img-responsive breadcrumb" typeof="foaf:Image">
+<?php endif; ?>
+</div>
